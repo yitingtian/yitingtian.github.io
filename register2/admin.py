19e61e9b63ae41630d06d320f245bc1eb2dd0e13
eve tian <105632012+yitingtian@users.noreply.github.com>
2023-01-12 23:59:44 +0800

    Delete register2 directory

--- register2/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import register
-
-admin.site.register(register)
--- register2/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Register2Config(AppConfig):
-    name = 'register2'
